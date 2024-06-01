e329d1e9e25fa251deb53357eee6467ace423644
한재준 <gkswowns25@naver.com>
2024-06-01 16:31:14 +0900

    Feat: 최종 성과 제출

---
+++ spring_thu/HanJaeJune/library-app/src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -0,0 +1,53 @@
+package com.group.libraryapp.controller.user;
+
+import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
+import com.group.libraryapp.dto.user.response.UserResponse;
+import com.group.libraryapp.service.user.UserServiceV1;
+import com.group.libraryapp.service.user.UserServiceV2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController // UserController 클래스를 진입 지점으로 만들어 주는 것 뿐만 아니라 Sptring bin에 등록시켜준다!
+public class UserController {
+
+    private final UserServiceV2 userService;
+    // private final FruitService fruitService;
+
+    // public UserController(UserService userService, @Qualifier("appleService") FruitService fruitService){
+    //        this.userService = userService;
+    //        this.fruitService = fruitService;
+    //    }
+
+    public UserController(UserServiceV2 userService){
+        this.userService = userService;
+    }
+    // private final List<User> users = new ArrayList<>();
+    @PostMapping("/user") // POST /user
+    public void saveUser(@RequestBody UserCreateRequest request) {
+        userService.saveUser(request);
+    }
+
+    @GetMapping("/user")
+    public List<UserResponse> getUsers() {
+        return userService.getUsers();
+    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        userService.updateUser(request);
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name) {
+        userService.deleteUser(name);
+    }
+
+
+//    @GetMapping("/fruit")
+//    public Fruit fruit() { // 겟터를 자동으로 인식해서 JSON형식으로 반환해준다. (RestController 어노테이션 때문)
+//        return new Fruit("바나나", 2000);
+//    }
+}

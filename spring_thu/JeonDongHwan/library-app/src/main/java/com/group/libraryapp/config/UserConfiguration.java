4e65d3cd3a611276de0fcd12e8b477156ab869d8
jdh7351 <jdh7351@naver.com>
2024-06-02 23:23:02 +0900

    Feat: 최종 성과 제출

---
+++ spring_thu/JeonDongHwan/library-app/src/main/java/com/group/libraryapp/config/UserConfiguration.java
@@ -0,0 +1,15 @@
+package com.group.libraryapp.config;
+
+//@Configuration
+//public class UserConfiguration {
+//
+//    @Bean
+//    public UserJdbcRepository userRepository(JdbcTemplate jdbcTemplate){
+//        return new UserJdbcRepository(jdbcTemplate);
+//    }
+//
+//    @Bean
+//    public UserService userService(UserJdbcRepository userRepository){
+//        return new UserService(userRepository);
+//    }
+//}
